7ab0edae52ff50843e458c1a7ee03fea074e3a4a
heejin <heejin@steppay.kr>
2023-12-21 18:26:11 +0900

    skip-validate-spec 추가, v1만 빌드되도록 수정

--- run.sh
+++ run.sh
@@ -8,7 +8,8 @@ yarn install
 echo "Fetching API specs in $ENV environment..."
 APP_ENV=$ENV yarn fetch
 
-tags=('v1' 'public' 'manager' 'customer' 'internal' 'admin' 'payment', 'all')
+# tags=('v1' 'public' 'manager' 'customer' 'internal' 'admin' 'payment', 'all')
+tags=('v1')
 for tag in "${tags[@]}"; do
     echo "Combining API spec for segment: $tag"
     API_SEGMENT=$tag yarn combine
